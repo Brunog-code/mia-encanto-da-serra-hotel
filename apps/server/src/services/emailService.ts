b7f8d178551f1715ec617a710af1142535b921fb
Bruno Gonçalves <brunogon.silva@hotmail.com>
2025-10-27 20:39:09 -0300

    fix: email sendgrid

--- apps/server/src/services/emailService.ts
+++ apps/server/src/services/emailService.ts
@@ -1,4 +1,6 @@
-import nodemailer from "nodemailer";
+import sgMail from "@sendgrid/mail";
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY as string);
 
 interface ISendEmail {
   to: string;
@@ -6,37 +8,19 @@ interface ISendEmail {
   html: string;
 }
 
-//function para criar o transporte
-const createTransporter = () => {
-  if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
-    throw new Error("Variáveis de ambiente do email não carregadas!");
-  }
-
-  return nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
-};
-
-//function para enviar email
 export const sendEmail = async ({ to, subject, html }: ISendEmail) => {
-  const transporter = createTransporter();
-  const mailOptions = {
-    from: process.env.EMAIL_USER,
-    to,
-    subject,
-    html,
-  };
-
   try {
-    return await transporter.sendMail(mailOptions);
-  } catch (error) {
-    console.error("Erro ao enviar email:", error);
+    const msg = {
+      to,
+      from: process.env.EMAIL_USER as string, // remetente verificado no SendGrid
+      subject,
+      html,
+    };
+
+    await sgMail.send(msg);
+    console.log("Email enviado com sucesso para:", to);
+  } catch (error: any) {
+    console.error("Erro ao enviar email:", error.response?.body || error);
     throw new Error("Erro ao enviar email");
   }
 };
